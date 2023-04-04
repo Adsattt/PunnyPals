381dc92c0876c155a5c8b5fcdfd5396ac341f8a7
Adi Satria <adisatria@apps.ipb.ac.id>
2023-04-04 13:06:49 +0700

    no message

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import SearchInput from './SearchInput';
 
 const Navbar:React.FC = () => {
     
@@ -7,8 +8,11 @@ const Navbar:React.FC = () => {
         <Flex bg="white" height='44px' padding='6px 12px'>
             <Flex align='center'>
                 <Image src="/images/redditFace.svg" height='30px' />
-                <Image src="/images/redditText.svg" height='46px'/>
+                <Image src="/images/redditText.svg" height='46px'display={{ base: "none", md: "unset"}}/>
             </Flex>
+            <SearchInput />
+            {/* <Directory />
+            <RightContent /> */}
         </Flex>
     )
 }
