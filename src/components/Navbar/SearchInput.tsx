6d496cedb102b29e19010fdf33a7035ca851a665
Adi Satria <adisatria@apps.ipb.ac.id>
2023-04-19 10:59:12 +0700

    Update navbar biar lebih responsif

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,24 +1,25 @@
 import { SearchIcon } from "@chakra-ui/icons";
-import {
-  Flex,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-} from "@chakra-ui/react";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { User } from "firebase/auth";
 import React from "react";
 
 type SearchInputProps = {
-  // user:
+  user?: User | null;
 };
 
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} mr={2} alignItems='center'>
+    <Flex
+      flexGrow={1}
+      mr={2}
+      ml={2}
+      maxWidth={user ? "auto" : "600px"}
+      alignItems="center"
+    >
       <InputGroup>
         <InputLeftElement
           pointerEvents="none"
-          children={<SearchIcon color="gray.400" mb={1}/>}
+          children={<SearchIcon color="gray.400" mb={1} />}
         />
         <Input
           placeholder="Search"
@@ -30,12 +31,12 @@ const SearchInput: React.FC<SearchInputProps> = () => {
             borderColor: "blue.500",
           }}
           _focus={{
-            outLine: 'none',
-            border: '1px solid',
-            borderColor: 'blue.500'
+            outLine: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
           }}
-          height='34px'
-          bg='gray.50'
+          height="34px"
+          bg="gray.50"
         />
       </InputGroup>
     </Flex>
