398068e667ba1ecc104eab55cbbc0564c496e24f
IFDHAAA\Khairurrijal <ifdhaulfitri77@gmail.com>
2023-05-13 09:44:29 +0700

    nambahin kolom pencarian

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,22 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import PageContent from '../components/Layout/PageContent';
+import CreatePostLink from '../components/Layout/CreatePostLink';
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>
+  return( 
+  <>
+    <PageContent>
+        <>
+        <CreatePostLink/>
+        </>
+        <>
+        <div>RHS</div>
+        </>
+    </PageContent>
+    </>
+  )
 };
 
 export default Home;
