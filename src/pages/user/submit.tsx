4c5b7286496d6bb5db78cca4a5a376520a0add55
Adi Satria <adisatria@apps.ipb.ac.id>
2023-05-18 18:08:26 +0700

    feat: add submit post

---
+++ src/pages/user/submit.tsx
@@ -0,0 +1,20 @@
+import PageContent from "@/src/components/Layout/PageContent";
+import NewPostForm from "@/src/components/Post/NewPostForm";
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+const submit: React.FC = () => {
+  return (
+    <PageContent>
+      <>
+        <Box padding="14px 0px" borderBottom="1px solid" borderColor="white">
+          <Text fontWeight={600} color="#33B6FF" fontSize="23px">
+            Create a post
+          </Text>
+        </Box>
+        <NewPostForm />
+      </>
+      <></>
+    </PageContent>
+  );
+};
+export default submit;
