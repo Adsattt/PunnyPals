5b4ec957a41984fb5828687453948e2d2625fff9
Adi Satria <adisatria@apps.ipb.ac.id>
2023-06-02 07:51:17 +0700

    Add icon for category menu

--- src/components/Post/PostForm/CategoryMenu.tsx
+++ src/components/Post/PostForm/CategoryMenu.tsx
@@ -1,11 +1,12 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
   Flex,
+  Image,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
-  Text,
+  Text
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 
@@ -18,7 +19,35 @@ const CategoryMenu: React.FC<CategoryMenuProps> = ({ onSelectCategory }) => {
 
   const handleSelectCategory = (category: string) => {
     setSelectedCategory(category);
-    onSelectCategory(category); // Call the onSelectCategory prop with the selected category
+    onSelectCategory(category);
+  };
+
+  const renderCategoryContent = (category: string) => {
+    switch (category) {
+      case "General":
+        return (
+          <>
+            <Image src="/icons/general.svg" mr={2} height="12px" />
+            <Text fontSize="10pt">General</Text>
+          </>
+        );
+      case "Ilkom":
+        return (
+          <>
+            <Image src="/icons/ilkom.svg" mr={2} height="12px" />
+            <Text fontSize="10pt">Ilkom</Text>
+          </>
+        );
+      case "PKU":
+        return (
+          <>
+            <Image src="/icons/pku.svg" mr={2} height="12px" />
+            <Text fontSize="10pt">PKU</Text>
+          </>
+        );
+      default:
+        return null;
+    }
   };
 
   return (
@@ -37,7 +66,7 @@ const CategoryMenu: React.FC<CategoryMenuProps> = ({ onSelectCategory }) => {
           borderColor: "blue.500",
         }}
         _focus={{
-          outLine: "none",
+          outline: "none",
           border: "1px solid",
           borderColor: "blue.500",
         }}
@@ -45,29 +74,66 @@ const CategoryMenu: React.FC<CategoryMenuProps> = ({ onSelectCategory }) => {
       >
         <Flex align="center" justify="space-between">
           <Flex align="center" fontWeight={700}>
-            <Text fontSize="8pt">Category</Text>
+            {selectedCategory ? (
+              renderCategoryContent(selectedCategory)
+            ) : (
+              <Text fontSize="8pt">Category</Text>
+            )}
           </Flex>
           <ChevronDownIcon />
         </Flex>
       </MenuButton>
       <MenuList>
         <MenuItem
+          fontSize="12pt"
+          fontWeight={600}
           onClick={() => handleSelectCategory("General")}
-          color={selectedCategory === "General" ? "blue.500" : "inherit"} // Mengubah warna teks jika menu dipilih
+          color={selectedCategory === "General" ? "blue.500" : "inherit"}
+          _hover={{ bg: "blue.400", color: "white" }}
+          sx={{
+            "&:hover img": {
+              filter: "brightness(0) invert(1)",
+            },
+          }}
         >
-          General
+          <Flex direction="row" align="center" justifyItems="center">
+            <Image src="/icons/general.svg" mr={2} height="16px" />
+            <Text mb={1}>General</Text>
+          </Flex>
         </MenuItem>
         <MenuItem
+          fontSize="12pt"
+          fontWeight={600}
           onClick={() => handleSelectCategory("Ilkom")}
-          color={selectedCategory === "Ilkom" ? "blue.500" : "inherit"} // Mengubah warna teks jika menu dipilih
+          color={selectedCategory === "Ilkom" ? "blue.500" : "inherit"}
+          _hover={{ bg: "blue.400", color: "white" }}
+          sx={{
+            "&:hover img": {
+              filter: "brightness(0) invert(1)",
+            },
+          }}
         >
-          Ilkom
+          <Flex direction="row" align="center" justifyItems="center">
+            <Image src="/icons/ilkom.svg" mr={2} height="16px" />
+            <Text>Ilkom</Text>
+          </Flex>
         </MenuItem>
         <MenuItem
+          fontSize="12pt"
+          fontWeight={600}
           onClick={() => handleSelectCategory("PKU")}
-          color={selectedCategory === "PKU" ? "blue.500" : "inherit"} // Mengubah warna teks jika menu dipilih
+          color={selectedCategory === "PKU" ? "blue.500" : "inherit"}
+          _hover={{ bg: "blue.400", color: "white" }}
+          sx={{
+            "&:hover img": {
+              filter: "brightness(0) invert(1)",
+            },
+          }}
         >
-          PKU
+          <Flex direction="row" align="center" justifyItems="center">
+            <Image src="/icons/pku.svg" mr={2} height="16px" />
+            <Text>PKU</Text>
+          </Flex>
         </MenuItem>
       </MenuList>
     </Menu>
